96dab40bc8c4ce50c9d952f63fd8a822b45fc17d
jAguilar2000 <52142265+jAguilar2000@users.noreply.github.com>
2023-11-21 00:28:20 -0600

    Crud-usuarios

---
+++ EasyPos/Controllers/UsuariosController.cs
@@ -0,0 +1,74 @@
+﻿using EasyPos.Models;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+
+namespace EasyPos.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        private EasyPosEntities db = new EasyPosEntities();
+
+        public ActionResult Index()
+        {
+            return View(db.Usuario.ToList());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "usuarioId,usuario1,nombre,password,email,estado")] Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                usuario.estado = true;
+                db.Usuario.Add(usuario);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(usuario);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Usuario usuario = db.Usuario.Find(id);
+            if (usuario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(usuario);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "usuarioId,usuario1,nombre,password,email,estado")] Usuario usuario)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(usuario).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(usuario);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
