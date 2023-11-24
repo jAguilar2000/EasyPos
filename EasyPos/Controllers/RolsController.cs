9d6f471ddea0cfb5cda97d50e8044faf57fcf510
jAguilar2000 <52142265+jAguilar2000@users.noreply.github.com>
2023-11-23 23:01:44 -0600

    CRUD-ROLES

---
+++ EasyPos/Controllers/RolsController.cs
@@ -0,0 +1,74 @@
+﻿using EasyPos.Models;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Mvc;
+
+namespace EasyPos.Controllers
+{
+    public class RolsController : Controller
+    {
+        private EasyPosEntities db = new EasyPosEntities();
+
+        public ActionResult Index()
+        {
+            return View(db.Rol.ToList());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "rolId,descripcion,estado")] Rol rol)
+        {
+            if (ModelState.IsValid)
+            {
+                rol.estado = true;
+                db.Rol.Add(rol);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(rol);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Rol rol = db.Rol.Find(id);
+            if (rol == null)
+            {
+                return HttpNotFound();
+            }
+            return View(rol);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "rolId,descripcion,estado")] Rol rol)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(rol).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(rol);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
