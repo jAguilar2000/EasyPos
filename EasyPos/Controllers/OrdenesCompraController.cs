fb534c54f8525fb77591e5ad85e7daa1ca8e2d0c
anjimenez21 <81935849+anjimenez21@users.noreply.github.com>
2023-11-27 00:36:15 -0600

    Creacion de vista y controlador orden de compras

---
+++ EasyPos/Controllers/OrdenesCompraController.cs
@@ -0,0 +1,25 @@
+﻿using EasyPos.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EasyPos.Controllers
+{
+    public class OrdenesCompraController : Controller
+    {
+        // GET: OrdenesCompra
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+    }
+}
