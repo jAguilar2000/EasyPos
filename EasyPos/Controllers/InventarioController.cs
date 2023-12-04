64704632a6d559ac1b026b8692ea10483a396590
kevinA67 <kfunes599@gmail.com>
2023-12-03 21:02:51 -0600

    Inventario de producto

---
+++ EasyPos/Controllers/InventarioController.cs
@@ -0,0 +1,67 @@
+﻿using EasyPos.Models;
+using EasyPos.Models.ViewModels;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web.Configuration;
+using System.Web.Mvc;
+
+namespace EasyPos.Controllers
+{
+    public class InventarioController : Controller
+    {
+        private EasyPosEntities db = new EasyPosEntities();
+
+        public ActionResult Index()
+        {
+            return View(db.Inventario.ToList());
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "inventarioId,productoId,stockDisponible,stockMin,stockMax,estado")] Inventario inventario)
+        {
+            if (ModelState.IsValid)
+            {
+                inventario.estado = true;
+                db.Inventario.Add(inventario);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(inventario);
+        }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Inventario inventario = db.Inventario.Find(id);
+            if (inventario == null)
+            {
+                return HttpNotFound();
+            }
+            return View(inventario);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "inventarioId,productoId,stockDisponible,stockMin,stockMax,estado")] Inventario inventario)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(inventario).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(inventario);
+        }
+    }
+}
---
+++ EasyPos/Models/ViewModels/InventarioVM.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace EasyPos.Models.ViewModels
+{
+    public class InventarioVM
+    {
+        public List<Inventario> InventarioList { get; set; }
+
+        public int productoId { get; set; }
+        public Producto descripcion { get; set; }
+        //public UsuarioRol UsuarioRol { get; set; }
+        public Inventario Inventario { get; set; }
+
+        public Producto Producto { get; set; }
+
+        public List<Producto> ProductoList { get; set; }
+
+        //public bool estado { get; set; }
+
+     
+    }
+}
